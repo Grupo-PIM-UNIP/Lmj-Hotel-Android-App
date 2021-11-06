c1affb712ccea12cc0148c2316169f41eff68dd4
Jean Carlos <jean.silva0096@gmail.com>
2021-11-06 02:39:44 -0300

    Criando tela Home
    Co-authored-by: name <luiz.eduardo.sp@outlook.com>
    Co-authored-by: name <luana.maximo027@gmail.com>
    Co-authored-by: name <mariana.masinha@gmail.com>

--- AppLmjHotel/app/src/main/java/com/unip/applmjhotel/MainActivity.java
+++ AppLmjHotel/app/src/main/java/com/unip/applmjhotel/MainActivity.java
@@ -1,13 +1,30 @@
 package com.unip.applmjhotel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnTelaHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTelaHome = findViewById(R.id.btnHomeView);
+        abrirOutraTela(btnTelaHome);
+    }
+
+    private void abrirOutraTela(Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HomeView.class));
+            }
+        });
     }
 }
